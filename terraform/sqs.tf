8b00402f1d86cbe884107a51ea3acca6eda99e58
David Arnold <dja+djaboxx@sonic.net>
2018-10-17 11:55:28 -0700

    Update sqs.tf

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -1,5 +1,5 @@
 resource "aws_sqs_queue" "reaper_queue" {
-  name                      = "WorkspaceReaper-${var.TFE_ORG}"
+  name                      = "WorkspaceReaper-${lower(var.TFE_ORG)}"
   max_message_size          = 2048
   message_retention_seconds = 86400
   receive_wait_time_seconds = 10
